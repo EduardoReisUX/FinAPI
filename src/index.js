5d65f4f7ec336e04dc6915eb66f6e161e15e3f20
EduardoReisUX <ducabruno09@hotmail.com>
2022-08-05 10:18:15 -0400

    feat: add middleware to verify if account exists

--- src/index.js
+++ src/index.js
@@ -7,12 +7,21 @@ app.use(express.json());
 
 const customers = [];
 
-/**
- * cpf - string
- * name - string
- * id - uid
- * statement []
- */
+// Middleware
+function verifyIfExistsAccountCPF(request, response, next) {
+  const { cpf } = request.headers;
+
+  const customer = customers.find((customer) => customer.cpf === cpf);
+
+  if (!customer) {
+    return response.status(400).json({ error: "Customer not found" });
+  }
+
+  request.customer = customer;
+
+  return next();
+}
+
 app.post("/account", (request, response) => {
   const { cpf, name } = request.body;
 
@@ -34,15 +43,13 @@ app.post("/account", (request, response) => {
   return response.status(201).send();
 });
 
-app.get("/statement", (request, response) => {
-  const { cpf } = request.headers;
-
-  const customer = customers.find((customer) => customer.cpf === cpf);
-
-  if (!customer) {
-    return response.status(400).json({ error: "Customer not found" });
-  }
-
+/* 
+app.get("/statement", middleware1, middleware2, ..., (req, res) => {}) 
+OU
+app.use(verifyIfExistsAccountCPF)
+*/
+app.get("/statement", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
   return response.json(customer.statement);
 });
 
